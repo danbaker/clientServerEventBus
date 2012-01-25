3ea3e6f9691146f7f2a6c3d99d3b3d59432ef642
Dan Baker <dan.baker@domo.com>
2012-01-24 20:38:02 -0700

    Got socket.io functioning nicely

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var app = express.createServer();
 var io = require('socket.io').listen(app);
 
 // Configuration
+io.configure(function(){
+  io.set('log level', 1);
+});
 
 app.configure(function(){
   app.set('views', __dirname + '/www/views');
@@ -37,9 +40,42 @@ app.get('/', function(req, res){
 app.listen(8765);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
+// // // // // // // // // // // // // // // // //
+//
+//	 	Handle Socket.IO
+//
+var allConnections = {};
+var nConnections = 0;
+
+// new client just connected
+var doConnect = function(socket) {
+	if (socket && socket.id && !allConnections[socket.id]) {
+		allConnections[socket.id] = {};
+		nConnections++;
+		console.log("New Connection.  client id="+socket.id+"  total connections="+nConnections);
+	} else {
+		console.log("ERROR: Same client connection ID used twice");
+	}
+};
+//existing client just disconnected
+var doDisconnect = function(socket) {
+	if (socket && socket.id) {
+		delete allConnections[socket.id];
+		nConnections--;
+		console.log("Disconnection.  client id="+socket.id+"  total connections="+nConnections);
+	} else {
+		console.log("ERROR: unknown client disconnected");
+	}
+};
+
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+	doConnect(socket);
+	socket.emit('news', { hello: 'world' });
+
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+	socket.on('disconnect', function () {
+		doDisconnect(socket);
+	});
 });
